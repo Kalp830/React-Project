00d091214c26b7c5a0b3959d9edaf084b4318d5b
Kalpesh Talekar <kalpesh.talekar830@gmail.com>
2024-07-11 19:22:52 +0530

    todolist

--- 09_Todolist/src/components/todo.jsx
+++ 09_Todolist/src/components/todo.jsx
@@ -1,14 +1,42 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './style.css'
+
+     // local storage data back
+     const getLocaData=()=>{
+        const lists = localStorage.getItem("mytodolist");
+          if(lists){
+            return JSON.parse(lists);
+          }
+          else{
+            return[];
+          }
+      };
 
 const Todo = () => {
     const [inputdata, setInputData] = useState("");
-    const [items, setInputItems] = useState([]);
+    const [items, setInputItems] = useState(getLocaData());
+    const [isEditItem, setIsEditItem]= useState("");
+    const [toggleButton, setToggleButton] = useState(false);
 
     const addItems=()=>{
        if(!inputdata){
          alert("plz fill the data")
-       } else{
+       }
+       else if(inputdata && toggleButton){
+         setInputItems(
+           items.map((currEle)=>{
+            if(currEle.id === isEditItem){
+                return{...currEle, name: inputdata};
+            }
+            return currEle;
+           })
+
+         ); 
+        setInputData([])
+        setIsEditItem(null);
+        setToggleButton(false);
+       }
+        else{
         const myNewInputData = {
             id:new Date().getTime().toString(),
             name:inputdata
@@ -23,7 +51,28 @@ const Todo = () => {
              return currEle.id !== index;
            });
            setInputItems(updateItem);
-    }
+    };
+
+    // edit items 
+    const editItem=(index)=>{
+        const item_todo_edited = items.find((currEle)=>{
+            return currEle.id === index;
+        });
+        setInputData(item_todo_edited.name)
+        setIsEditItem(index);
+        setToggleButton(true);
+     }
+    // remov all elememts
+
+    const removeAll=()=>{
+       setInputItems([]);
+    };
+
+    // adding local storage
+       useEffect(()=>{
+         localStorage.setItem("mytodolist", JSON.stringify(items))
+       }, [items]);
+
 
   return (
     <>
@@ -40,9 +89,11 @@ const Todo = () => {
                 className='form-control'
                 value={inputdata}
                 onChange={(event)=> (setInputData(event.target.value))}
-                
                 />
-                <i className='fa fa-plus add-btn' onClick={addItems}></i>
+                {
+                    toggleButton ? (<i className='far fa-edit add-btn' onClick={addItems}></i>) : (<i className='fa fa-plus add-btn' onClick={addItems}></i>) 
+                }
+               
             </div>
             {/* show our items */}
              <div className="showItems">
@@ -52,7 +103,7 @@ const Todo = () => {
                             <div className="eachItem" key={index}>
                                 <h3>{currEle.name}</h3>
                                 <div className="todo-btn">
-                                <i className='far fa-edit add-btn'></i>
+                                <i className='far fa-edit add-btn' onClick={()=> editItem(currEle.id)}></i>
                                 <i className='far fa-trash-alt add-btn' onClick={()=> deleteItem(currEle.id)}></i>
                                 </div>
                             </div>
@@ -63,7 +114,7 @@ const Todo = () => {
 
             {/* remove all button */}
             <div className="showItems">
-                <button className='btn effect04' data-sm-link-text="Remove All"><span>Check List</span></button>
+                <button className='btn effect04' data-sm-link-text="Remove All" onClick={removeAll}><span>Check List</span></button>
             </div>
         </div>
     </div>
