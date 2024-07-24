d4cad1d02d8b2ddaba17a8cc6b20fcec09fedb04
kalpesh <talekar.kt@gmail.com>
2024-07-25 00:07:29 +0530

    Few more code

--- 10Covid-Website/src/components/pages/Home-Sections/SympOfCorona.jsx
+++ 10Covid-Website/src/components/pages/Home-Sections/SympOfCorona.jsx
@@ -10,35 +10,49 @@ const SympOfCorona = () => {
                     <p className='text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
                 </div>
             <div className='row'>
-                <div className='col-xl-6 col-md-6 col'>
+                <div className='col-xl-5 col-md-5 col-12'>
                 <CardDetail
                   imgURL='./images/symptom-img.png'
                   title='High Fever'
                   description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doua. Ut enim a in reprehenderit in.'
                 />
                 </div>
-                <div className='col-xl-6 col-md-6 col'>
+                <div className='col-xl-1 col-md-1 col'></div>
+                <div className='col-xl-5 col-md-5 col-12'>
                 <CardDetail
                 imgURL='./images/symptom-img.png'
                 title='Cough'
                 description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doua. Ut enim a in reprehenderit in.'
                 />
                 </div>
-                <div className='col-xl-6 col-md-6 col'>
+                <div className='col-xl-1 col-md-1 col'></div>
+                <div className='col-xl-5 col-md-5 col-12'>
                 <CardDetail
                 imgURL='./images/symptom-img.png'
                 title='Sore Troth'
                 description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doua. Ut enim a in reprehenderit in.'
                 />
                 </div>
-                <div className='col-xl-6 col-md-6 col'>
+                <div className='col-xl-1 col-md-1 col'></div>
+                <div className='col-xl-5 col-md-5 col-12'>
                 <CardDetail
                 imgURL='./images/symptom-img.png'
                 title='Headache'
                 description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doua. Ut enim a in reprehenderit in.'
                 />
                 </div>
+                <div className='col-xl-1 col-md-1 col'></div>
                
+            </div>
+
+            <div className='row'>
+                <div className='col-xl-6 col-md col-lg'>
+                  <h5>Stay at home and call your doctor : Lorem ipsum dolor sit amet, consectetur adipiscing elit
+sed do eiusmod tempor  </h5>
+                </div>
+                <div className='col-xl-2 offset-xl-2 offset-md-0 offset-lg-2 col-md col-lg'>
+                <button className='btn btn-prevent text-white'>Help Line</button>
+                </div>
             </div>
           </div>
           </section>
@@ -54,7 +68,7 @@ const SympOfCorona = () => {
             <div className='col-xl-auto col'>
             <img src={imgURL}/>
             </div> 
-            <div className='col-xl-8'>
+            <div className='col-xl'>
             <h2>{title}</h2>
             <p>{description}</p>
             </div> 
