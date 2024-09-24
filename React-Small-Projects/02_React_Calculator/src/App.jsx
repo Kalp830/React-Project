53ff3aef8c2daf0e314a22facc783755a2e5bb36
Kalpesh Talekar <kalpesh.talekar830@gmail.com>
2024-09-24 19:09:27 +0530

    React Calculator App

--- React-Small-Projects/02_React_Calculator/src/App.jsx
+++ React-Small-Projects/02_React_Calculator/src/App.jsx
@@ -2,18 +2,19 @@
 import style from './App.module.css';
 
 function App() {
-
+ const number = ['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.']
 
   return (
     <>
      <div className='container-sm w-25 mt-5'>
-      <div className="row">
+      <h1 className={style.h1}> React Calculator App </h1>
         <div className={style.calculator}>
-            <input className="form-control" id="display" type="text" />
-            <div className="button-container">
-               <button className="btn btn-outline-primary">C</button>
+            <input className={`${style["display"]}`}  type="text" />
+            <div className={style.buttonContainer}>
+              {number.map((item)=>(
+                <button key={item} className={`${style["button"]} btn btn-outline-primary`}>{item}</button>
+              ))}
             </div>
-        </div>
         </div>
      </div>
     </>
