0fe989e892cc124d61b4b3e4e5e1141f12dd026e
kalpesh <talekar.kt@gmail.com>
2024-12-02 21:59:39 +0530

    Added loop in netflix

--- React-Small-Projects/03_Netflix_Series_Card/src/assets/components/NetflixSeries.jsx
+++ React-Small-Projects/03_Netflix_Series_Card/src/assets/components/NetflixSeries.jsx
@@ -3,20 +3,26 @@ import seriesData from '../../api/seriesData.json';
 const NetflixSeries = () => {
   return (
     <>
+<ul>
+ {seriesData.map((curEle) =>(
+<li key={curEle.id}>
       <img
-       src={seriesData[0].img_url}
-       alt="img"
+       src={curEle.img_url}
+       alt={curEle.name}
        width="40%"
        height="40%"
       ></img>
-      <h2>Name: {seriesData[0].name}</h2>
-      <h3>Rating: {seriesData[0].rating}</h3>
-      <p>Description: {seriesData[0].description}</p>
-      <p>Genre: {seriesData[0].genre}</p>
-      <p>Cast: {seriesData[0].cast}</p>
-      <a href={seriesData[0].watch_url}>
-        <button>Watch Now</button>
+      <h2>Name: {curEle.name}</h2>
+      <h3>Rating: {curEle.rating}</h3>
+      <p>Description: {curEle.description}</p>
+      <p>Genre: {curEle.genre}</p>
+      <p>Cast: {curEle.cast}</p>
+      <a href={curEle.watch_url}>
+        <button style={{marginBottom:"10px"}}>Watch Now</button>
       </a>
+      </li>
+      ))};
+      </ul>
     </>
   )
 }
