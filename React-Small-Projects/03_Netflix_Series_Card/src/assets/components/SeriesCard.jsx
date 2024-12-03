92eeeea9793e83f2303aa506558cc5299af77f4a
kalpesh <talekar.kt@gmail.com>
2024-12-03 22:42:44 +0530

    Passing Props

--- React-Small-Projects/03_Netflix_Series_Card/src/assets/components/SeriesCard.jsx
+++ React-Small-Projects/03_Netflix_Series_Card/src/assets/components/SeriesCard.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
 const SeriesCard = (props) => {
+  const {id, img_url, name, genre, rating, cast, watch_url, description} = props.curEle
   return (
     <>
-      <li key={props.curEle.id}>
+      <li key={id}>
       <img
-       src={props.curEle.img_url}
-       alt={props.curEle.name}
+       src={img_url}
+       alt={name}
        width="40%"
        height="40%"
       ></img>
-      <h2>Name: {props.curEle.name}</h2>
-      <h3>Rating: {props.curEle.rating}</h3>
-      <p>Description: {props.curEle.description}</p>
-      <p>Genre: {props.curEle.genre}</p>
-      <p>Cast: {props.curEle.cast}</p>
-      <a href={props.curEle.watch_url}>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>Description: {description}</p>
+      <p>Genre: {genre}</p>
+      <p>Cast: {cast}</p>
+      <a href={watch_url}>
         <button style={{marginBottom:"10px"}}>Watch Now</button>
       </a>
       </li> 
