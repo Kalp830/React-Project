9c8c1d62b55a3308100009f387388c52940fcf61
Kalpesh Talekar <kalpesh.talekar@acumen.aero>
2025-01-24 17:58:51 +0530

    Props Prectice

--- React-Small-Projects/03_Netflix_Series_Card/src/App.jsx
+++ React-Small-Projects/03_Netflix_Series_Card/src/App.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import EventProps from './assets/components/EventProps'
-import LiftingState from './assets/components/LiftingState'
-import NetflixSeries from './assets/components/NetflixSeries'
-import Profile from './assets/components/Profile'
-import State from './assets/components/State'
-import Todo from './assets/components/Todo/Todo'
-import ToggleSwitch from './assets/components/toggle-switch/ToggleSwitch'
+import { useState } from "react";
+import "./App.css";
+import EventProps from "./assets/components/EventProps";
+import LiftingState from "./assets/components/LiftingState";
+import NetflixSeries from "./assets/components/NetflixSeries";
+import Profile from "./assets/components/Profile";
+import State from "./assets/components/State";
+import Todo from "./assets/components/Todo/Todo";
+import ToggleSwitch from "./assets/components/toggle-switch/ToggleSwitch";
+import Card from "./assets/components/props/Card";
+import Button from "./assets/components/props/Button";
 
 function App() {
- 
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <>
-     {/* <NetflixSeries/>
+      {/* <NetflixSeries/>
       <Profile />
       <EventProps />
       <State/>
       <LiftingState /> */}
       {/* <ToggleSwitch /> */}
-      <Todo />
+      {/* <Todo /> */}
+      {/* <Card name="Love Coding">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          architecto!
+        </p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </Card> */}
+      <h1>{count}</h1>
+      <Button handleClick={handleClick} text="Click Me"></Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- React-Small-Projects/03_Netflix_Series_Card/src/assets/components/State.jsx
+++ React-Small-Projects/03_Netflix_Series_Card/src/assets/components/State.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import styled from "styled-components";
 
 const State = () => {
   const [count, setCount] = useState(5);
-  const [color, setColor] = useState('gray');
-    const handleButtonClick = () => {
-        setCount(()=> count + 1)
-  }
+  const [color, setColor] = useState("gray");
+  const handleButtonClick = () => {
+    setCount(() => count + 1);
+  };
   const handleColor = () => {
-    setColor("red")
-  }
+    setColor("red");
+  };
 
   const BtnColor = styled.div`
-     background: transparent;
-     border: 2px solid red;
-     width:100px;
-     height:auto;
-     cursor: pointer;
-     background:green;
-     color:white;
-     font-size: 14px;
-     padding: 5px;
+    background: transparent;
+    border: 2px solid red;
+    width: 100px;
+    height: auto;
+    cursor: pointer;
+    background: green;
+    color: white;
+    font-size: 14px;
+    padding: 5px;
   `;
 
   return (
     <>
       <h1>Value {count}</h1>
-      <div style={{ backgroundColor: color, width: "100px", height: "100px" }}></div>
-      <BtnColor onClick={()=> handleColor()}>Change Color</BtnColor>
+      <div
+        style={{ backgroundColor: color, width: "100px", height: "100px" }}
+      ></div>
+      <BtnColor onClick={() => handleColor()}>Change Color</BtnColor>
       <button onClick={handleButtonClick}>Increment Count</button>
-      
     </>
-  )
-}
+  );
+};
 
-export default State
+export default State;
