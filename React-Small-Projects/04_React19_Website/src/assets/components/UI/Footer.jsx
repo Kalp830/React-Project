893c31bd5601aabe51b6b36d0871cc5022b22328
Kalpesh <kalpesh.talekar@acumen.aero>
2025-02-07 15:50:06 +0530

    footer added

--- React-Small-Projects/04_React19_Website/src/assets/components/UI/Footer.jsx
+++ React-Small-Projects/04_React19_Website/src/assets/components/UI/Footer.jsx
@@ -1,7 +1,31 @@
+import footerContact from '../../../api/footerApi.json'
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
+
 const Footer = () => {
   return (
     <>
-      <h1 style={{ color: "#fff" }}>Footer</h1>
+      <footer className="footer-section">
+        <div className="container grid grid-three-cols">
+            {  
+              footerContact.map((item, index)=>{
+                const {icon, title, details} = item;
+                return(
+                  <>
+                  <div className="footer-contact" key={index}>
+                    <div className="icon">{icon}</div>
+                    <div className="footer-contact-text">
+                      <p>{title}</p>
+                      <p>{details}</p>
+                    </div>
+                  </div>
+                  </>
+                )
+              })
+            }
+        </div>
+      </footer>
     </>
   );
 };
