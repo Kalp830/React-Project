c77b902ecd7a28e413fc92181ffd3492571444ec
Kalpesh Talekar <kalpesh.talekar@acumen.aero>
2025-02-11 18:44:18 +0530

    Made country page

--- React-Small-Projects/04_React19_Website/src/assets/pages/Country.jsx
+++ React-Small-Projects/04_React19_Website/src/assets/pages/Country.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../../api/postApi";
+import Loader from "../components/UI/Loader";
+import CountryCard from "../components/Layout/CountryCard";
+
 const Country = () => {
-  return <div>Country</div>;
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) return <Loader />;
+
+  return (
+    <>
+      <section className="country-section">
+        <ul className="grid grid-four-cols">
+          {countries.map((curCountry, index) => {
+            return <CountryCard country={curCountry} key={index} />;
+          })}
+        </ul>
+      </section>
+    </>
+  );
 };
 
 export default Country;
